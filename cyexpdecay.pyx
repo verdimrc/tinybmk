47e1df0905af8c02e6e783e759cd0a9d7425b78a
Verdi March <verdi.march@gmail.com>
2017-03-15 19:49:45 +0800

    cpdef Cython functions

--- cyexpdecay.pyx
+++ cyexpdecay.pyx
@@ -1,8 +1,7 @@
 import math as pymath
 import sys
 
-cdef extern from 'math.h':
-    double exp(double)
+from libc.math cimport exp
 
 rng = xrange if sys.version_info.major < 3 else range
 
@@ -11,7 +10,7 @@ def cy_expdecay(i, x=0.5, factor=1.0):
         y = pymath.exp(-factor*x)
     return y
 
-def cya_expdecay(int i, double x=0.5, double factor=1.0):
+cpdef double cya_expdecay(int i, double x=0.5, double factor=1.0):
     cdef int _
     cdef double y
     for _ in rng(i):
@@ -22,7 +21,7 @@ def cy_expdecay2(x=0.5, factor=1.0):
     y = pymath.exp(-factor*x)
     return y
 
-def cya_expdecay2(double x=0.5, double factor=1.0):
+cpdef double cya_expdecay2(double x=0.5, double factor=1.0):
     cdef double y
     y = exp(-factor*x)
     return y
